f68210e82eac6d1f64659c2ff6f57eaace74a5af
Gal Tashma <galtashma@gmail.com>
2019-04-22 13:28:27 +0800

    Removed npm dependencies, converting them to git subtree

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    //...
+    resolve: {
+        alias: {
+            // configuration options
+            // 'vorpal-less': require.resolve('node_modules/vorpal-less/dist'),
+            // 'vorpal-grep': require.resolve('node_modules/vorpal-grep')
+        }
+    }
+  };
